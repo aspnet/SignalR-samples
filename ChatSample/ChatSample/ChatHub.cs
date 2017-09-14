dc4812a8ff71e2a7511acd96bbe748888ffe9d8d
Pawel Kadluczka <pawelka@microsoft.com>
2017-09-14 14:39:35 -0700

    Adding Chat Sample

---
+++ ChatSample/ChatSample/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatSample
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.InvokeAsync("broadcastMessage", name, message);
+        }
+    }
+}
