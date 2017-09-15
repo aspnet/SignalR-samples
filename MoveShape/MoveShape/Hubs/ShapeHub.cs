3fa53ac067f47f3e85c54b7c11a03ad20f19214e
Mikael Mengistu <mimengis@microsoft.com>
2017-09-15 15:13:38 -0700

    MoveShape sample

---
+++ MoveShape/MoveShape/Hubs/ShapeHub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MoveShape.Hubs
+{
+    public class ShapeHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public async Task MoveShape(int x , int y)
+        {
+            //This will be simpler once there is support for Others or when AllExcept has a params overload
+            var excludedIds = new List<string>();
+            excludedIds.Add(Context.ConnectionId);
+            await Clients.AllExcept(excludedIds).InvokeAsync("shapeMoved", x, y);
+        }
+    }
+}
---
+++ MoveShape/MoveShape/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using MoveShape.Hubs;
+
+namespace MoveShape
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseFileServer();
+
+            app.UseSignalR(router =>
+            {
+                router.MapHub<ShapeHub>("shapeHub");
+            });
+        }
+    }
+}
