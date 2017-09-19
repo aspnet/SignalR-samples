320f23676a748aefc5b0008b9fb9829dacd6b2a9
Mikael Mengistu <mimengis@microsoft.com>
2017-09-19 16:40:17 -0700

    Add support for concurrent drawing

--- WhiteBoard/WhiteBoard/Hubs/DrawHub.cs
+++ WhiteBoard/WhiteBoard/Hubs/DrawHub.cs
@@ -6,24 +6,9 @@ namespace WhiteBoard.Hubs
 {
     public class DrawHub : Hub
     {
-        public Task MouseUp()
+        public Task Draw(int prevX, int prevY, int x, int y, string color)
         {
-            return Clients.AllExcept(new List<string> { Context.ConnectionId }).InvokeAsync("mouse-up");
-        }
-
-        public Task MouseDown()
-        {
-            return Clients.AllExcept(new List<string> { Context.ConnectionId }).InvokeAsync("mouse-down");
-        }
-
-        public Task MouseOut()
-        {
-            return Clients.All.InvokeAsync("mouse-out");
-        }
-
-        public Task Draw(int x, int y, string color)
-        {
-            return Clients.AllExcept(new List<string> { Context.ConnectionId }).InvokeAsync("draw", x, y, color);
+            return Clients.AllExcept(new List<string> { Context.ConnectionId }).InvokeAsync("draw", prevX, prevY, x, y, color);
         }
     }
 }
