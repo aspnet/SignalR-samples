4b9451595668ce829c501872bf1c0e479e59afea
Pawel Kadluczka <pawelka@microsoft.com>
2017-10-09 16:06:23 -0700

    Reconnect sample

--- StockTickR/CsharpClient/Program.cs
+++ StockTickR/CsharpClient/Program.cs
@@ -16,15 +16,6 @@ namespace CsharpClient
 
         static async Task Run()
         {
-            var connection = new HubConnectionBuilder()
-                .WithUrl("http://localhost:5000/stocks")
-                .WithConsoleLogger()
-                .WithMessagePackProtocol()
-                .WithTransport(TransportType.WebSockets)
-                .Build();
-
-            await connection.StartAsync();
-
             Console.WriteLine("Starting connection. Press Ctrl-C to close.");
             var cts = new CancellationTokenSource();
             Console.CancelKeyPress += (sender, a) =>
@@ -33,33 +24,81 @@ namespace CsharpClient
                 cts.Cancel();
             };
 
-            connection.Closed += e =>
-            {
-                Console.WriteLine("Connection closed with error: {0}", e);
-
-                cts.Cancel();
-                return Task.CompletedTask;
-            };
-
-            connection.On("marketOpened", async () =>
-            {
-                await StartStreaming();
-            });
-
-            // Do an initial check to see if we can start streaming the stocks
-            var state = await connection.InvokeAsync<string>("GetMarketState");
-            if (string.Equals(state, "Open"))
-            {
-                await StartStreaming();
-            }
+            await StartConnection(cts);
 
             // Keep client running until cancel requested.
             while (!cts.IsCancellationRequested)
             {
                 await Task.Delay(250);
             }
+        }
 
-            async Task StartStreaming()
+        private async static Task StartConnection(CancellationTokenSource cts)
+        {
+            var transports = new [] { TransportType.WebSockets, TransportType.ServerSentEvents, TransportType.LongPolling };
+            HubConnection connection = null;
+            foreach (var transport in transports)
+            {
+                try
+                {
+                    connection = await StartConnection(transport, cts);
+                    Console.WriteLine($"Connected using {transport.ToString()} transport.");
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"Could not connect to server using {transport.ToString()} transport.");
+                    if (transport == TransportType.LongPolling)
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            // Do an initial check to see if we can start streaming the stocks
+            var state = await connection.InvokeAsync<string>("GetMarketState");
+            if (string.Equals(state, "Open"))
+            {
+                await StartStreaming(connection, cts);
+            }
+        }
+
+        private async static Task<HubConnection> StartConnection(TransportType transportType, CancellationTokenSource cts)
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/stocks")
+                // .WithConsoleLogger()
+                .WithMessagePackProtocol()
+                .WithTransport(transportType)
+                .Build();
+
+            connection.On("marketOpened", async () =>
+            {
+                await StartStreaming(connection, cts);
+            });
+
+            connection.Closed += async e =>
+            {
+                if (!await Reconnect(cts))
+                {
+                    Console.WriteLine($"Connection closed with error: {e} and reconnecting failed.");
+                    cts.Cancel();
+                }
+                else
+                {
+                    Console.WriteLine("Client reconnected successfully.");
+                }
+
+                await connection.DisposeAsync();
+            };
+
+            await connection.StartAsync();
+            return connection;
+        }
+
+        private async static Task StartStreaming(HubConnection connection, CancellationTokenSource cts)
+        {
+            try
             {
                 var channel = await connection.StreamAsync<Stock>("StreamStocks", CancellationToken.None);
                 while (await channel.WaitToReadAsync() && !cts.IsCancellationRequested)
@@ -70,6 +109,28 @@ namespace CsharpClient
                     }
                 }
             }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error receiving results: {ex.Message}");
+            }
+        }
+
+        private async static Task<bool> Reconnect(CancellationTokenSource cts)
+        {
+            for (var i = 0; i <= 5 && !cts.IsCancellationRequested; i++)
+            {
+                Console.WriteLine($"Reconnecting... Attempt {i}");
+                try
+                {
+                    await StartConnection(cts);
+                    return true;
+                }
+                catch
+                {
+                }
+                await Task.Delay(TimeSpan.FromSeconds(5));
+            }
+            return false;
         }
     }
 
