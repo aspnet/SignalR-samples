d72cb6460054a86f884ae013f79cd32ad3a17056
unknown <mimengis@microsoft.com>
2018-04-20 00:41:55 -0700

    Update MoveShape sample to preview2

--- MoveShape/MoveShape/Hubs/ShapeHub.cs
+++ MoveShape/MoveShape/Hubs/ShapeHub.cs
@@ -16,7 +16,7 @@ namespace MoveShape.Hubs
         public async Task MoveShape(int x , int y)
         {
             //This will be simpler once there is support for Others or when AllExcept has a params overload
-            await Clients.AllExcept(new List<string> { Context.ConnectionId }).InvokeAsync("shapeMoved", x, y);
+            await Clients.AllExcept(new List<string> { Context.ConnectionId }).SendAsync("shapeMoved", x, y);
         }
     }
 }
