5d99fce00eee4aa80456f17a9bd138a449507316
unknown <mimengis@microsoft.com>
2018-04-20 11:30:34 -0700

    clean up

--- MoveShape/MoveShape/Hubs/ShapeHub.cs
+++ MoveShape/MoveShape/Hubs/ShapeHub.cs
@@ -8,11 +8,6 @@ namespace MoveShape.Hubs
 {
     public class ShapeHub : Hub
     {
-        public override Task OnConnectedAsync()
-        {
-            return base.OnConnectedAsync();
-        }
-
         public async Task MoveShape(int x , int y)
         {
             await Clients.Others.SendAsync("shapeMoved", x, y);
