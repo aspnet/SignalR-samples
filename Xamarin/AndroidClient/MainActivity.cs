a55e32f0cb897d53a2d22d0bd627431a487e275a
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-06-05 17:21:51 -0700

    xamarin still in sad times :(

--- Xamarin/AndroidClient/MainActivity.cs
+++ Xamarin/AndroidClient/MainActivity.cs
@@ -3,7 +3,9 @@ using Android.Widget;
 using Android.OS;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.SignalR.Protocol;
 
 namespace AndroidApp
 {
@@ -12,80 +14,88 @@ namespace AndroidApp
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(savedInstanceState);
+            MakeProtocol();
 
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            //base.OnCreate(savedInstanceState);
 
-            //Setting up references to views
-            var connectButton = FindViewById<Button>(Resource.Id.bConnect);
-            var listView = FindViewById<ListView>(Resource.Id.lvMessages);
-            var sendButton = FindViewById<Button>(Resource.Id.bSend);
-            var messageText = FindViewById<EditText>(Resource.Id.etMessageText);
-            var connected = false;
+            //// Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.activity_main);
 
-            var hubConnection = new HubConnectionBuilder()
-                    .WithUrl("<YOUR SERVER URL HERE>")
-                    .Build();
+            ////Setting up references to views
+            //var connectButton = FindViewById<Button>(Resource.Id.bConnect);
+            //var listView = FindViewById<ListView>(Resource.Id.lvMessages);
+            //var sendButton = FindViewById<Button>(Resource.Id.bSend);
+            //var messageText = FindViewById<EditText>(Resource.Id.etMessageText);
+            //var connected = false;
 
-            //Setting up window to enter name
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-            alertDialogBuilder.SetTitle("Enter Your Name");
+            //var hubConnection = new HubConnectionBuilder()
+            //        .WithUrl("http://localhost:5000/")
+            //        .Build();
 
-            var userName = "";
-            var nameInput = new EditText(this);
-            nameInput.InputType = Android.Text.InputTypes.TextVariationPersonName;
-            alertDialogBuilder.SetView(nameInput);
+            ////Setting up window to enter name
+            //AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            //alertDialogBuilder.SetTitle("Enter Your Name");
 
-            alertDialogBuilder.SetPositiveButton("Ok", (_, __) =>
-            {
-                userName = nameInput.Text;
-            });
+            //var userName = "";
+            //var nameInput = new EditText(this);
+            //nameInput.InputType = Android.Text.InputTypes.TextVariationPersonName;
+            //alertDialogBuilder.SetView(nameInput);
 
-            alertDialogBuilder.SetNegativeButton("Cancel", (_, __) => { });
+            //alertDialogBuilder.SetPositiveButton("Ok", (_, __) =>
+            //{
+            //    userName = nameInput.Text;
+            //});
 
-            alertDialogBuilder.Show();
+            //alertDialogBuilder.SetNegativeButton("Cancel", (_, __) => { });
 
-            var messages = new List<string>();
-            var arrayAdapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, messages);
-            listView.Adapter = arrayAdapter;
+            //alertDialogBuilder.Show();
 
-            connectButton.Click += async (sender, e) =>
-            {
-                await hubConnection.StartAsync();
-                connectButton.Enabled = false;
-                connected = true;
-                Toast.MakeText(this, "Connected", ToastLength.Short).Show();
-                await hubConnection.InvokeAsync("Send", userName, "is now connected");
-            };
+            //var messages = new List<string>();
+            //var arrayAdapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, messages);
+            //listView.Adapter = arrayAdapter;
 
-            hubConnection.On<string, string>("broadcastMessage", (name, message) =>
-            {
-                var newMessage = $"{name}: {message}";
-                RunOnUiThread(() => {
-                    arrayAdapter.Add(newMessage);
-                    arrayAdapter.NotifyDataSetChanged();
-                });
-            });
+            //connectButton.Click += async (sender, e) =>
+            //{
+            //    await hubConnection.StartAsync();
+            //    connectButton.Enabled = false;
+            //    connected = true;
+            //    Toast.MakeText(this, "Connected", ToastLength.Short).Show();
+            //    await hubConnection.InvokeAsync("Send", userName, "is now connected");
+            //};
 
-            sendButton.Click += async (sender, e) =>
-            {
-                if (!connected)
-                {
-                    Toast.MakeText(this, "Must be Connected to Send!", ToastLength.Short).Show();
-                    return;
-                }
+            //hubConnection.On<string, string>("broadcastMessage", (name, message) =>
+            //{
+            //    var newMessage = $"{name}: {message}";
+            //    RunOnUiThread(() => {
+            //        arrayAdapter.Add(newMessage);
+            //        arrayAdapter.NotifyDataSetChanged();
+            //    });
+            //});
 
-                try
-                {
-                    await hubConnection.InvokeAsync("Send", userName, messageText.Text);
-                    messageText.Text = "";
-                }
-                catch (Exception ex)
-                {
-                    var exception = ex;
-                }
-            };
+            //sendButton.Click += async (sender, e) =>
+            //{
+            //    if (!connected)
+            //    {
+            //        Toast.MakeText(this, "Must be Connected to Send!", ToastLength.Short).Show();
+            //        return;
+            //    }
+
+            //    try
+            //    {
+            //        await hubConnection.InvokeAsync("Send", userName, messageText.Text);
+            //        messageText.Text = "";
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        var exception = ex;
+            //    }
+            //};
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void MakeProtocol()
+        {
+            var proto = new JsonHubProtocol();
         }
     }
 }
